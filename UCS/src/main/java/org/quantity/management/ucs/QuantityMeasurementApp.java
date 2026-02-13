771387a221efc48b2c7f73b6d46d0692812680c3
Vimal Watwani <vimalwatwani10.it@gmail.com>
2026-02-13 13:46:32 +0530

    Changes for Bridgelabz training Use Case 2

--- UCS/src/main/java/org/quantity/management/ucs/QuantityMeasurementApp.java
+++ UCS/src/main/java/org/quantity/management/ucs/QuantityMeasurementApp.java
@@ -6,14 +6,14 @@ public class QuantityMeasurementApp {
 
     public static class Feet {
 
-        private final double value;
+        private final double feet;
 
-        public Feet(Double value) {
-            this.value = value;
+        public Feet(Double feet) {
+            this.feet = feet;
         }
 
-        public double getValue() {
-            return value;
+        public double getFeet() {
+            return feet;
         }
 
         @Override
@@ -25,13 +25,41 @@ public class QuantityMeasurementApp {
                 return false;
             }
             Feet feetObject = (Feet) obj;
-            return (Double.compare(this.value,feetObject.getValue()) == 0);
+            return (Double.compare(this.feet, feetObject.getFeet()) == 0);
+        }
+    }
+
+    public static class Inches {
+        private final double inches;
+
+        public Inches(Double inches) {
+            this.inches = inches;
+        }
+
+        public double getInches() {
+            return inches;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(this == obj) {
+                return true;
+            }
+            if(this.getClass() != obj.getClass()) {
+                return false;
+            }
+            Inches inchesObject = (Inches) obj;
+            return (Double.compare(this.inches, inchesObject.getInches()) == 0);
         }
     }
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Feet1 value : ");
         Double feet1 = scanner.nextDouble();
+
+        System.out.println("Enter Feet2 value : ");
         Double feet2 = scanner.nextDouble();
 
         Feet feetObject1 = new Feet(feet1);
@@ -40,7 +68,23 @@ public class QuantityMeasurementApp {
         if(feetObject1.equals(feetObject2)) {
             System.out.println("Feet 1 and Feet 2 are equal.");
         } else {
-            System.out.println("Feet 1 and Feet 2 are not equal");
+            System.out.println("Feet 1 and Feet 2 are not equal.");
         }
+
+        System.out.println("Enter Inches1 value : ");
+        Double inches1 = scanner.nextDouble();
+
+        System.out.println("Enter Inches2 value : ");
+        Double inches2 = scanner.nextDouble();
+
+        Inches inchesObject1 = new Inches(inches1);
+        Inches inchesObject2 = new Inches(inches2);
+
+        if(inchesObject1.equals(inchesObject2)) {
+            System.out.println("Inches 1 and Inches 2 are equal.");
+        } else {
+            System.out.println("Inches 1 and Inches 2 are not equal");
+        }
+        scanner.close();
     }
 }
--- UCS/src/test/java/org/quantity/management/ucs/QuantityMeasurementAppTest.java
+++ UCS/src/test/java/org/quantity/management/ucs/QuantityMeasurementAppTest.java
@@ -3,6 +3,7 @@ package org.quantity.management.ucs;
 import org.junit.Assert;
 import org.junit.Test;
 import org.quantity.management.ucs.QuantityMeasurementApp.Feet;
+import org.quantity.management.ucs.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -11,24 +12,41 @@ public class QuantityMeasurementAppTest {
         Feet feet1 = new Feet(Double.valueOf(10));
         Feet feet2 = new Feet(Double.valueOf(10));
         Assert.assertTrue(feet1.equals(feet2));
+
+        Inches inches1 = new Inches(Double.valueOf(10));
+        Inches inches2 = new Inches(Double.valueOf(10));
+        Assert.assertTrue(inches1.equals(inches2));
     }
     @Test
     public void testFeetEquality_DifferentValue() {
         Feet feet1 = new Feet(Double.valueOf(10));
         Feet feet2 = new Feet(Double.valueOf(11));
         Assert.assertFalse(feet1.equals(feet2));
+
+        Inches inches1 = new Inches(Double.valueOf(10));
+        Inches inches2 = new Inches(Double.valueOf(10));
+        Assert.assertTrue(inches1.equals(inches2));
     }
     @Test
     public void testFeetEquality_NullComparison() {
         Feet feet1 = new Feet(Double.NaN);
         Feet feet2 = new Feet(Double.NaN);
         Assert.assertTrue(feet1.equals(feet2));
+
+        Inches inches1 = new Inches(Double.valueOf(10));
+        Inches inches2 = new Inches(Double.valueOf(10));
+        Assert.assertTrue(inches1.equals(inches2));
+
     }
     @Test
     public void testFeetEquality_DifferentClass() {
         Feet feet1 = new Feet(Double.valueOf(10));
         QuantityMeasurementApp feet2 = new QuantityMeasurementApp();
         Assert.assertFalse(feet1.equals(feet2));
+
+        Inches inches1 = new Inches(Double.valueOf(10));
+        Inches inches2 = new Inches(Double.valueOf(10));
+        Assert.assertTrue(inches1.equals(inches2));
     }
     @Test
     public void testFeetEquality_SameReference() {
