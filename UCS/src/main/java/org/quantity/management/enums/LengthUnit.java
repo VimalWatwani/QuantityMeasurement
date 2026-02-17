63b32e3dd70f935d39d9fc2b0b49677f6f808102
Vimal Watwani <vimalwatwani10.it@gmail.com>
2026-02-17 16:52:57 +0530

    Changes for Use case 4

--- UCS/src/main/java/org/quantity/management/enums/LengthUnit.java
+++ UCS/src/main/java/org/quantity/management/enums/LengthUnit.java
@@ -3,7 +3,9 @@ package org.quantity.management.enums;
 public enum LengthUnit {
 
     FEET(12.0),
-    INCHES(1.0);
+    INCHES(1.0),
+    YARDS(36),
+    CENTIMETERS(0.393701);
 
     private final double conversionFactor;
 
--- UCS/src/main/java/org/quantity/management/ucs/Ucs3Main.java
+++ UCS/src/main/java/org/quantity/management/ucs/Ucs3Main.java
@@ -10,5 +10,13 @@ public class Ucs3Main {
         Length length1 = new Length(1.0, LengthUnit.FEET);
         Length length2 = new Length(12.0, LengthUnit.INCHES);
         System.out.println("Are Lengths equal ? + " + length1.equals(length2));
+
+        Length length3 = new Length(1.0, LengthUnit.YARDS);
+        Length length4 = new Length(36.0, LengthUnit.INCHES);
+        System.out.println("Are Lengths equal ? + " + length3.equals(length4));
+
+        Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
+        Length length6 = new Length(39.37010, LengthUnit.INCHES);
+        System.out.println("Are Lengths equal ? + " + length5.equals(length6));
     }
 }
